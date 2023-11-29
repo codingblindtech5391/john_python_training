a663690ed21baf58d2a7e1c7fac57e8347fc7474
kblackwelder <kblackwelder08@gmail.com>
2023-11-28 22:04:39 -0500

    new updates to guess the number game for john

--- python_learning/keith_to_john/v1_guess_number_game.py
+++ python_learning/keith_to_john/v1_guess_number_game.py
@@ -14,21 +14,22 @@ print(f"You get {guesses_allowed} guesses. The number will be between 0 and 100.
 
 
 while guesses_taken < guesses_allowed:
-    user_input = input(int("Please enter your guess."))
-    guesses_taken += 1
+    user_input = int(input("Please enter your guess."))
+
     if user_input > random_number:
-        print("Guess to high. Guess again.")
+        print("Guess to high.")
     elif user_input < random_number:
-        print("Guess to low. Guess again.")
+        print("Guess to low.")
     else:
-        if guesses_allowed >= guesses_taken:
-            # Out of guesses
-            print("you did not win.")
-            break
-        else:
-            # won the game
-            print("Great job! You won the game.")
+        # won the game
+        print("Great job! You won the game.")
+        break
 
+    guesses_taken += 1
 
+# Check if guesses taken are equal to or greater than guesses_allowed
+# If so, then game lost.
+if guesses_taken >= guesses_allowed:
+    print(f"Sorry you did not guess the number. The number was { random_number }")
 
 
